0164e37c3d64caadd053aa0aaa81a14618621d16
Li Jing <jingli9111@gmail.com>
2017-10-18 12:47:02 -0400

    recall T=100

--- recall.sh
+++ recall.sh
@@ -1,7 +1,6 @@
-python recall_task.py RUM -H 50
-python recall_task.py RUM -H 100
-python recall_task.py LSTM -H 50
-python recall_task.py LSTM -H 100
+python recall_task.py RUM -T 100
+python recall_task.py LSTM -T 50
+python recall_task.py LSTM -T 100
 
 
 
